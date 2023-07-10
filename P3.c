8601484162513eec04807dab2d54cd19eee1eec5
josue riera <josueriera23@hotmail.com>
2023-07-10 14:59:49 -0500

    2 version

--- P3.c
+++ P3.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define maximo 100
+
+typedef struct {
+    char code[maximo];
+    char name[maximo];
+    float nota;
+} estudiante;
 
 int main() {
-    // Write C code here
+    
     printf("Hello world");
 
     return 0;
