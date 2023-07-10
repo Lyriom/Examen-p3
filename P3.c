50909adb10f91d51b22bc1de6f8861cea37fc6d3
Lyriom <130936689+Lyriom@users.noreply.github.com>
2023-07-10 15:33:07 -0500

    4

--- P3.c
+++ P3.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LONGITUD 100
+#define max 100
 
 typedef struct {
-    char codigo[MAX_LONGITUD];
-    char nombre[MAX_LONGITUD];
+    char codigo[max];
+    char nombre[max];
     float nota;
 } Estudiante;
-
+float calcularPromedioNotas(Estudiante* estudiantes, int cantidad) {
+    float suma = 0.0;
+    for (int i = 0; i < cantidad; i++) {
+        suma += estudiantes[i].nota;
+    }
+    return suma / cantidad;
+}
 // Función de comparación para qsort
 int compararEstudiantes(const void* a, const void* b) {
     const Estudiante* estudianteA = (const Estudiante*)a;
